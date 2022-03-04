3877520f280e4ea2dabc56fa1a89d9f4b3668344
Brad Jorsch <anomie@users.sourceforge.net>
2022-03-04 16:33:50 -0500

    Test

--- .github/files/test-plugin-update.sh
+++ .github/files/test-plugin-update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eo pipefail
+
 DIR="$PWD/plugins"
 EXIT=0
 
@@ -17,27 +19,32 @@ for FROM in stable dev; do
 
             # Update the plugin.
             echo "::group::Upgrading $slug via $HOW"
+            P="$(wp --allow-root plugin path "$slug" | sed 's!^/var/www/html/wp-content/plugins/!!')"
+            wp --allow-root --quiet option set fake_plugin_update_plugin "$P"
             wp --allow-root --quiet option set fake_plugin_update_url "$DIR/$slug-dev.zip"
+            : > /var/www/html/wp-content/debug.log
             if [[ "$HOW" == 'cli' ]]; then
-                if ! wp --allow-root plugin upgrade "$slug" 2>&1 | tee out.txt; then
-                    echo "::error::CLI upgrade exited with a non-zero status"
+                if ! wp --allow-root plugin upgrade "$slug" 2>&1 | tee "$DIR/out.txt"; then
+                    echo "::error::CLI upgrade of $slug from $FROM exited with a non-zero status"
                     EXIT=1
                 fi
             else
-                P="$(wp --allow-root plugin path "$slug" | sed 's!^/var/www/html/wp-content/plugins/!!')"
-                curl -v --get --url 'http://localhost/wp-admin/update.php?action=upgrade-plugin&_wpnonce=bogus' --data "plugin=$P" --output out.txt 2>&1
-                cat out.txt
+                chown -R www-data:www-data /var/www/html
+                curl -v --get --url 'http://localhost/wp-admin/update.php?action=upgrade-plugin&_wpnonce=bogus' --data "plugin=$P" --output "$DIR/out.txt" 2>&1
+                cat "$DIR/out.txt"
             fi
+            echo '== Debug log =='
+            cat /var/www/html/wp-content/debug.log
+            wp --allow-root --quiet option delete fake_plugin_update_plugin
             wp --allow-root --quiet option delete fake_plugin_update_url
             echo "::endgroup::"
-            if [[ ! -e "/var/www/html/wp-content/plugins/$slug/ci-flag.txt" ]]; then
-                echo "::error::Plugin does not seem to have been updated?"
+            ERR="$(grep -i 'Fatal error' "$DIR/out.txt" || true)"
+            if [[ -n "$ERR" ]]; then
+                echo "::error::Mid-upgrade fatal detected for $slug $HOW update from $FROM!%0A$ERR"
                 EXIT=1
-            else
-                ERR="$(grep 'Fatal error' out.txt || true)"
-                if [[ -n "$ERR" ]]; then
-                    echo "::error::Mid-upgrade fatal detected!%0A$ERR"
-                fi
+            elif [[ ! -e "/var/www/html/wp-content/plugins/$slug/ci-flag.txt" ]]; then
+                echo "::error::Plugin $slug ($HOW update from $FROM) does not seem to have been updated?"
+                EXIT=1
             fi
 
             echo "::group::Uninstalling $slug"
